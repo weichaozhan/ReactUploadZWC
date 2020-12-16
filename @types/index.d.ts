11709e5ba23d23cfd16c2a24140f10345888a8af
wczhan <921391554@qq.com>
2020-12-16 20:05:58 +0800

    upload dir

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,5 +1,9 @@
 declare namespace ReactUploadZWC {
   interface IUploadProps {
+    accept?: string;
+    action?: string| ((file: File) => Promise);
+    method?: 'post' | 'get';
+    directory?: boolean;
     onChange?: (e: React.ChangeEvent<HTMLInputElement>) => any; 
   }
 }
--- src/Index.tsx
+++ src/Index.tsx
@@ -1,11 +1,24 @@
-import React, { FC, useRef } from 'react';
+import React, { FC, useEffect, useRef } from 'react';
 
 import styles from './Index.scss';
 
-const Upload: FC<ReactUploadZWC.IUploadProps> = ({
+type TProps = ReactUploadZWC.IUploadProps;
+
+const Upload: FC<TProps> = ({
+  accept,
+  directory = true,
   onChange
 }) => {
-  const fileLabel: React.MutableRefObject<HTMLInputElement | null> = useRef(null);
+  const fileInputFile: any = useRef(null);
+
+  useEffect(() => {
+    if (fileInputFile.current) {
+      (fileInputFile.current as (HTMLInputElement & {
+        webkitdirectory: boolean
+      })).webkitdirectory = directory;
+    }
+  }, [fileInputFile.current]);
+  
   const changeFile = (e) => {
     onChange?.(e);
   };
@@ -13,7 +26,13 @@ const Upload: FC<ReactUploadZWC.IUploadProps> = ({
   return <div className={styles['wrapper']} >
     <label className={styles['button-upload']} >
       上传文件
-      <input ref={fileLabel} className={styles['file-input']} type="file" onChange={changeFile} />
+      <input
+        accept={accept}
+        className={styles['file-input']}
+        ref={fileInputFile}
+        type="file"
+        onChange={changeFile}
+      />
     </label>
   </div>;
 };
